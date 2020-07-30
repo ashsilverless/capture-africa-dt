a87961a154600183488f6dba967f484f6c2c2b79
Ash - SIlverless <ash@silverless.co.uk>
2020-07-30 14:55:07 +0100

    FIX - misc mobile adjustments

--- page-templates/terms-privacy.php
+++ page-templates/terms-privacy.php
@@ -9,20 +9,33 @@ get_header();?>
 
 
 <div class="page__grid">
-    <div class="grid__location--content">
-        <h1 class="type__scale--2"><?php the_field('main_title');?></h1>
-    </div>
     <nav class="page-navigation grid__location--sidebar" role="navigation" aria-labelledby="page-navigation-label">
         <h2 id="page-navigation-label">Legal pages</h2>
         <ul class="page-navigation__list">
-            <li class="page-navigation__list-item page-navigation__list-item--current">
-                <a href="/privacy-policy">Privacy Policy</a>
+            <li class="page-navigation__list-item">
+                <a href="/privacy-policy" class="<?php
+                    global $post;
+                    $post_slug = $post->post_name;
+                    if($post_slug == 'privacy-policy') {
+                        echo 'current-page';
+                    }
+                    ?>">Privacy Policy</a>
             </li>
             <li class="page-navigation__list-item">
-                <a href="/terms">Terms &amp; Conditions</a>
+                <a href="/terms" class="<?php
+                    global $post;
+                    $post_slug = $post->post_name;
+                    if($post_slug == 'terms') {
+                        echo 'current-page';
+                    }
+                    ?>">Terms &amp; Conditions</a>
             </li>
         </ul>
     </nav>
+    <div class="grid__location--content">
+        <h1 class="type__scale--2"><?php the_field('main_title');?></h1>
+    </div>
+
     <div class="grid__location--content">
         <?php the_field('terms_copy');?>
     </div>
